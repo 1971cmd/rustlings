8daecd23d942817576f120a6425b094d5474eb13
Rashed AlKhajeh 100058714 <alkhajehrashed@gmail.com>
2025-03-05 11:10:22 +0400

    Update hashmaps2.rs

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -21,17 +21,15 @@ enum Fruit {
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     let fruit_kinds = [
-        Fruit::Apple,
         Fruit::Banana,
-        Fruit::Mango,
-        Fruit::Lychee,
         Fruit::Pineapple,
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+        // Insert new fruits if they are not already present in the basket.
+        if !basket.contains_key(&fruit) {
+            basket.insert(fruit, 1); // Add at least one of each new fruit
+        }
     }
 }
 
