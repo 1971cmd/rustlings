5c2f8ff5c0135d0c9f572a90743a40b32fe4577b
Rashed AlKhajeh 100058714 <alkhajehrashed@gmail.com>
2025-03-17 11:03:03 +0400

    Update lifetimes3.rs

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -1,6 +1,5 @@
-// Lifetimes are also needed when structs hold references.
-
-// TODO: Fix the compiler errors about the struct.
+// Add a lifetime parameter `'a` to the struct to specify that the references
+// `author` and `title` must live at least as long as the struct.
 struct Book<'a> {
     author: &'a str,
     title: &'a str,
