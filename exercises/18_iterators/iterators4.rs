ab3c947941ae6428acc68c326f0471336c4dd5a8
Rashed AlKhajeh 100058714 <alkhajehrashed@gmail.com>
2025-04-07 10:17:17 +0400

    Update iterators4.rs

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -10,6 +10,8 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+    
+    (1..=num).fold(1, |acc, x| acc * x)
 }
 
 fn main() {
@@ -19,21 +21,22 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn factorial_of_0() {
         assert_eq!(factorial(0), 1);
     }
-
+    
     #[test]
     fn factorial_of_1() {
         assert_eq!(factorial(1), 1);
     }
+    
     #[test]
     fn factorial_of_2() {
         assert_eq!(factorial(2), 2);
     }
-
+    
     #[test]
     fn factorial_of_4() {
         assert_eq!(factorial(4), 24);
